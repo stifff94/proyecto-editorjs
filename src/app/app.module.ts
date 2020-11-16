43e2540c1a2c3f65359b5d3fbcb73f78b6ccebbb
esteban carrera <esteban.icc@gmail.com>
2020-11-16 11:47:11 -0500

    commit ingreso usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditorComponent } from './editor/editor.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { AuthModule } from '@auth0/auth0-angular';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditorComponent
+    EditorComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    PreciosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-0hcvw6nu.us.auth0.com',
+      clientId: 'AYn95S1rF8jCE8ZaYpWnLRw8RAMvkkkB'
+    }),
+    NgbModule
+    // Import the module into the application, with configuration
   ],
   providers: [],
   bootstrap: [AppComponent]
